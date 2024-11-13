56d8d98851e386214299ae6b6b9be258e4ad968a
류정원 <ryuzy@kakao.com>
2024-11-13 15:51:00 +0900

    토큰 검증

--- data/auth.js
+++ data/auth.js
@@ -2,7 +2,7 @@ let users = [
     {
         id:'1',
         username:'apple',
-        password:'1111',
+        password:'$2b$10$pgJ/lpeHhtG9cYGGgKq.U.9.iZ/hX1j.kgVp/qTpDembvr.0XhNKK',
         name:'김사과',
         email:'apple@apple.com',
         url:'https://png.pngtree.com/thumb_back/fh260/background/20230611/pngtree-woman-s-face-with-long-brown-eyes-image_2888808.jpg'
@@ -10,7 +10,7 @@ let users = [
     {
         id:'2',
         username:'banana',
-        password:'2222',
+        password:'$2b$10$klvYHmNBMvLJQbiRbYFCt.mj2li27XjsToNP9gHl.fYPDFOu.DTQK',
         name:'반하나',
         email:'banana@banana.com',
         url:'https://png.pngtree.com/thumb_back/fh260/background/20230611/pngtree-woman-s-face-with-long-brown-eyes-image_2888808.jpg'
@@ -18,7 +18,7 @@ let users = [
     {
         id:'3',
         username:'orange',
-        password:'3333',
+        password:'$2b$10$XHe2BZnunhm.53Dwwvx8.e7rtgrbu4viFE55uKLdpNYcgDXqDnUM2',
         name:'오렌지',
         email:'orange@orange.com',
         url:'https://png.pngtree.com/thumb_back/fh260/background/20230611/pngtree-woman-s-face-with-long-brown-eyes-image_2888808.jpg'
@@ -28,10 +28,10 @@ let users = [
 export async function createUser(username, password, name, email){
     const user = {
         id:'4',
-        username:username,
-        password:password,
-        name:name,
-        email:email,
+        username,
+        password,
+        name,
+        email,
         url:'https://png.pngtree.com/thumb_back/fh260/background/20230611/pngtree-woman-s-face-with-long-brown-eyes-image_2888808.jpg'
     }
     users = [user, ...users]
@@ -41,4 +41,8 @@ export async function createUser(username, password, name, email){
 export async function findByUsername(username){
     const user = users.find((user) => user.username === username)
     return user
+}
+
+export async function findById(id){
+    return users.find((user) => user.id === id)
 }
